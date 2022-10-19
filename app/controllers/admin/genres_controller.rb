1c6a0b6fb556c3fcf59d91ff7dfa81981304454c
0611NASKA <010611naoki@gmail.com>
2022-10-19 14:34:14 +0900

    [Add]ジャンル一覧作成

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -1,2 +1,27 @@
 class Admin::GenresController < ApplicationController
+  before_action :authenticate_admin!
+
+  def index
+    @genres = Genre.all
+    @genre = Genre.new
+  end
+
+  def create
+    @genre = Genre.new(genre_params)
+    if @genre.save
+      redirect_to admin_genres_path, notice: "You have created genre successfully."
+    else
+      @genres = Genre.all
+      render 'index'
+    end
+  end
+
+  def edit
+  end 
+
+  private
+
+  def genre_params
+    params.require(:genre).permit(:name)
+  end
 end
