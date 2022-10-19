9e71b180856c35f48a0ccaae921ebfbbee2edffa
0611NASKA <010611naoki@gmail.com>
2022-10-19 18:06:51 +0900

    [Add]管理者/商品一覧

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,3 +1,30 @@
 class Admin::ItemsController < ApplicationController
   before_action :authenticate_admin!
+
+  def index
+    @items = Item.all
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def show
+    @item = Item.find(params[:id])
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to admin_item_path, notice: "商品の作成に成功しました"
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :introduction, :price, :is_active)
+  end
 end
