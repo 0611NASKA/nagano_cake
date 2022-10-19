7fb032d4fa29313ce2dc5a06747b139e764dcafc
0611NASKA <010611naoki@gmail.com>
2022-10-19 14:01:49 +0900

    [Add]会員/商品一覧作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,4 +3,12 @@ class Item < ApplicationRecord
   has_many :cart_items, dependent: :destroy
   belongs_to :genre
   has_many :order_details, dependent: :destroy
+
+  def get_image
+    (image.attached?) ? image : 'no_image.jpg'
+  end
+
+  def add_tax_price
+    (self.price * 1.08).round
+  end
 end
