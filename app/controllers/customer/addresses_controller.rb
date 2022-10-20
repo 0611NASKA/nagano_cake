6ee4418b8de1e5f88c2f40beba16f375ea03cf9f
0611NASKA <010611naoki@gmail.com>
2022-10-21 01:57:44 +0900

    [Finish]配送先機能

---
+++ app/controllers/customer/addresses_controller.rb
@@ -0,0 +1,44 @@
+class Customer::AddressesController < ApplicationController
+  before_action :authenticate_customer!
+
+  def index
+    @addresses = Address.all
+    @address = Address.new
+  end
+
+  def create
+    @address = Address.new(address_params)
+    @address.customer_id = current_customer.id
+    if @address.save
+      redirect_to addresses_path, notice: "住所の作成に成功しました"
+    else
+      @addresses = Address.all
+      render 'index'
+    end
+  end
+
+  def edit
+    @address = Address.find(params[:id])
+  end
+
+  def update
+    @address = Address.find(params[:id])
+    if @address.update(address_params)
+      redirect_to addresses_path, notice: "住所の編集に成功しました"
+    else
+      render "edit"
+    end
+  end
+
+  def destroy
+    @address = Address.find(params[:id])
+    @address.destroy
+    redirect_to addresses_path
+  end
+
+  private
+
+  def address_params
+    params.require(:address).permit(:customer_id, :name, :postal_code, :address)
+  end
+end
