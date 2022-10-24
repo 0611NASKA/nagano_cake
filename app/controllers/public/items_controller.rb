b7f4cc0f96bcd530ca18993d8090e17bb1f6df8c
0611NASKA <010611naoki@gmail.com>
2022-10-24 11:45:30 +0900

    [Finish]ジャンル検索

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,8 +1,15 @@
 class Public::ItemsController < ApplicationController
   before_action :authenticate_customer!, except: [:index]
-  
+
   def index
     @items = Item.all
+    @genres = Genre.all
+    if params[:genre_id]
+		  @genre = Genre.find(params[:genre_id])
+		  @items = @genre.items.where(is_active: true)
+    else
+      @items = Item.where(is_active: true)
+    end
   end
 
   def show
